5bed61d00c4bd682d2d193a83924afd6c2b391e4
Owen Griffin <owen.griffin@gmail.com>
2010-03-16 15:01:09 +0000

    Added twitter search and user feed

--- lib/who-needs-wp/twitter.rb
+++ lib/who-needs-wp/twitter.rb
@@ -1,9 +1,20 @@
 
 module WhoNeedsWP
   def self.twitter
+    @logger.debug @options[:twitter]
     if @options[:twitter][:user]
+      @logger.debug "Reading Tweets for user #{@options[:twitter][:user]}"
       @sidebar << @template['twitter'].render(Object.new, { 
-                                                :tweets => Twitter::Search.new(@options[:twitter][:user]).per_page(5).fetch().results, 
+                                                :tweets => Twitter::Search.new.from(@options[:twitter][:user]).per_page(5).fetch().results, 
+                                                :feed_title => "@#{@options[:twitter][:user]}",
+                                                :options => @options
+                                              })
+    end
+    if @options[:twitter][:search]
+      @logger.debug "Searching Twitter for #{@options[:twitter][:search]}"
+      @sidebar << @template['twitter'].render(Object.new, { 
+                                                :tweets => Twitter::Search.new(@options[:twitter][:search]).per_page(5).fetch().results, 
+                                                :feed_title => "Tweets for \"#{@options[:twitter][:search]}\"",
                                                 :options => @options
                                               })
     end
