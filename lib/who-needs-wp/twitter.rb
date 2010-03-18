60ca373e4c2cd41a4348ecc6a6411901a466dc50
Owen Griffin <owen.griffin@gmail.com>
2010-03-18 08:26:47 +0000

    Adds links to tweets

--- lib/who-needs-wp/twitter.rb
+++ lib/who-needs-wp/twitter.rb
@@ -1,11 +1,18 @@
 
 module WhoNeedsWP
+  
+  def self.parse_tweet(text)
+    text = text.gsub(/(http:\/\/[^ ]+)/, '<a href="\1">\1</a>')
+    text = text.gsub(/(@([^ :]+))/, '<a href="http://twitter.com/\2">\1</a>')
+    return text.gsub(/(#([^ :]+))/, '<a href="http://twitter.com/#search?q=%23\2">\1</a>')
+  end
+  
   def self.twitter
     @logger.debug @options[:twitter]
     if @options[:twitter][:user]
       @logger.debug "Reading Tweets for user #{@options[:twitter][:user]}"
       @sidebar << @template['twitter'].render(Object.new, { 
-                                                :tweets => Twitter::Search.new.from(@options[:twitter][:user]).per_page(5).fetch().results, 
+                                                :tweets => Twitter::Search.new.from(@options[:twitter][:user]).per_page(5).fetch().results,  
                                                 :feed_title => "@#{@options[:twitter][:user]}",
                                                 :options => @options
                                               })
