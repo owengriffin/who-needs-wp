833f94d485c2cf02e2593632cc947d1d718f84ba
Owen <owen.griffin@gmail.com>
2010-04-14 08:22:35 +0100

    First sitemap code

---
+++ lib/who-needs-wp/sitemap.rb
@@ -0,0 +1,29 @@
+
+module WhoNeedsWP
+  def self.sitemap
+    document = REXML::Document.new
+    urlset = document.add_element("urlset", { "xmlns" => "http://www.sitemaps.org/schemas/sitemap/0.9"})
+    Post.all.each do |post|
+      url = REXML::Element.new("url")
+      urlset.add(url)
+      url.add_element("loc").text = post.url
+      url.add_element("lastmod").text = post.created_at.strftime('%Y-%m-%d')
+      url.add_element("priority").text = "0.5"
+    end
+    Page.all.each do |page|
+      url = REXML::Element.new("url")
+      urlset.add(url)
+      url.add_element("loc").text = page.url
+      url.add_element("priority").text = "0.8"
+    end
+    
+    # Generate sitemap for index page
+    url = REXML::Element.new("url")
+    
+    url.add_element("loc").text = WhoNeedsWP::options[:url] + "index.html"
+    url.add_element("lastmod").text = Post.all[0].created_at.strftime('%Y-%m-%d')
+    url.add_element("priority").text = "1.0"
+    urlset.add(url)
+    puts document
+  end
+end
