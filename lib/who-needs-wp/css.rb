7600df2c7d14aaacce84aef7e4b7aa0359fbfe41
Owen Griffin <owen.griffin@gmail.com>
2010-08-29 10:56:16 +0100

    Added a reset_stylesheet option. Allows interpretation of user SASS stylesheets

--- lib/who-needs-wp/css.rb
+++ lib/who-needs-wp/css.rb
@@ -1,12 +1,25 @@
 
 module WhoNeedsWP
   def self.css(filename="style.css")
-    if @options[:stylesheet] == nil or @options[:stylesheet].empty?
+
+
+    # The folder which contains the default who-needs-wp stylesheets
     stylesheets_folder = File.expand_path(File.join(File.dirname(__FILE__), "stylesheets"))
-    @logger.debug "Generating stylesheets from #{stylesheets_folder}"
-    File.open(filename, "w") do |file|
-      file.puts Sass::Engine.new(File.read("#{stylesheets_folder}/style.sass"), { :load_paths => [stylesheets_folder]}).to_css
+
+    sass = ""
+    
+    if @options[:reset_stylesheet]
+      @logger.info "Including CSS reset stylesheet"
+      sass = File.read("#{stylesheets_folder}/style.sass")
     end
+
+    if @options[:stylesheet] != nil and not @options[:stylesheet].empty?
+      @logger.info "Including #{@options[:stylesheet]}"
+      sass = sass + "@import \"#{@options[:stylesheet]}\""
+    end
+
+    File.open(filename, "w") do |file|
+      file.puts Sass::Engine.new(sass, { :load_paths => [stylesheets_folder, Dir.pwd]}).to_css
     end
   end
 end
